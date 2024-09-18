3843948864ea2e9ceeb48248f6662399786e6d86
AHTOXA <tonerxdd@gmail.com>
2024-09-18 13:24:03 +0700

    Tasks updated with rules

--- LR1/task4-1.py
+++ LR1/task4-1.py
@@ -1,17 +1,19 @@
 """Программа для нахождения минимального значения в списке"""
 
 n = int(input())
+if n <= 0:
+    print("Ошибка: n не может быть <= 0")
+else:
+    nums = []
 
-nums = []
+    for i in range(n):
+        nums.append(int(input()))
 
-for i in range(n):
-    nums.append(int(input()))
+    min = nums[0]
+    index = 0
+    for i in range(1, n, 1):
+        if nums[i] < min:
+            min = nums[i]
+            index = i
 
-min = nums[0]
-index = 0
-for i in range(1, n, 1):
-    if nums[i] < min:
-        min = nums[i]
-        index = i
-
-print("min =", min, "at index", index)
+    print("Минимальный элемент", min, "найден, его индекс -", index)
--- LR1/task4-2.py
+++ LR1/task4-2.py
@@ -1,23 +1,24 @@
 """Программа для разделения списка значений по параметрам: больше нуля, меньше или равно нулю"""
 
 n = int(input())
+if n <= 0:
+    print("Ошибка: n не может быть <= 0")
+else:
+    nums = []
+    positive = []
+    other = []
 
-nums = []
-positive = []
-other = []
+    for i in range(n):
+        nums.append(int(input()))
+    print(nums)
 
-for i in range(n):
-    nums.append(int(input()))
-print(nums)
+    for i in range(n):
+        if nums[i] > 0:
+            positive.append(nums[i])
+    print(positive)
 
+    for i in range(n):
+        if nums[i] <= 0:
+            other.append(nums[i])
 
-for i in range(n):
-    if nums[i] > 0:
-        positive.append(nums[i])
-print(positive)
-
-for i in range(n):
-    if nums[i] <= 0:
-        other.append(nums[i])
-
-print(other)
+    print(other)
