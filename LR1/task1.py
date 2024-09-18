a47fda5c32ee9c6ca9e996b5b602605a3997398f
AHTOXA <tonerxdd@gmail.com>
2024-09-18 13:16:14 +0700

    Tasks 1 and 2 updated with rules

--- LR1/task1.py
+++ LR1/task1.py
@@ -1,27 +1,43 @@
-"""Программа для расчета и нахождения минимального расстояния между точками"""
-
 from math import sqrt
 
-def distance(x1, y1, x2, y2):
-    result = 0
-    result = pow(x2 - x1, 2) + pow(y2 - y1, 2)
-    result = sqrt(result)
-    return result
+x0 = int(input())
+y0 = int(input())
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+x3 = int(input())
+y3 = int(input())
+x4 = int(input())
+y4 = int(input())
 
+min_val = sqrt(pow(x1 - x0, 2) + pow(y1 - y0, 2))
 
-x = []
-y = []
+if sqrt(pow(x2 - x0, 2) + pow(y2 - y0, 2)) < min_val:
+    min_val = sqrt(pow(x2 - x0, 2) + pow(y2 - y0, 2))
 
-for num in range(5):
-    x.append(int(input()))
-    y.append(int(input()))
+if sqrt(pow(x3 - x0, 2) + pow(y3 - y0, 2)) < min_val:
+    min_val = sqrt(pow(x3 - x0, 2) + pow(y3 - y0, 2))
 
-min = distance(x[0], y[0], x[1], y[1])
-i = 0
-for i in range(4):
-    num = distance(x[i], y[i], x[i + 1], y[i + 1])
-    if num < min:
-        min = num
-    i = i + 1
+if sqrt(pow(x4 - x0, 2) + pow(y4 - y0, 2)) < min_val:
+    min_val = sqrt(pow(x4 - x0, 2) + pow(y4 - y0, 2))
 
-print(min)
+if sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2)) < min_val:
+    min_val = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2))
+
+if sqrt(pow(x3 - x1, 2) + pow(y3 - y1, 2)) < min_val:
+    min_val = sqrt(pow(x3 - x1, 2) + pow(y3 - y1, 2))
+
+if sqrt(pow(x4 - x1, 2) + pow(y4 - y1, 2)) < min_val:
+    min_val = sqrt(pow(x4 - x1, 2) + pow(y4 - y1, 2))
+
+if sqrt(pow(x3 - x2, 2) + pow(y3 - y2, 2)) < min_val:
+    min_val = sqrt(pow(x3 - x2, 2) + pow(y3 - y2, 2))
+
+if sqrt(pow(x4 - x2, 2) + pow(y4 - y2, 2)) < min_val:
+    min_val = sqrt(pow(x4 - x2, 2) + pow(y4 - y2, 2))
+
+if sqrt(pow(x4 - x3, 2) + pow(y4 - y3, 2)) < min_val:
+    min_val = sqrt(pow(x4 - x3, 2) + pow(y4 - y3, 2))
+
+print(min_val)
